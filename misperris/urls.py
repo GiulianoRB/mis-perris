ec1bb18d307caf940464e751d3c284ebe0ffaac9
drakaryss <giuliano.developer@gmail.com>
2018-11-17 01:32:56 -0300

    primer commit

---
+++ misperris/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import url, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
+    url(r'^', include('perrisapp.urls')),
+    url(r'^formulario/', include('perrisapp.urls')),
+    path('', include('pwa.urls')),
+]
