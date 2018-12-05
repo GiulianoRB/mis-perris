4858efb6128aa6c901e1a1ab498d6170da8ca57c
GiulianoRB <giuliano.developer@gmail.com>
2018-12-05 13:55:06 -0300

    ...

--- misperris/settings.py
+++ misperris/settings.py
@@ -204,3 +204,29 @@ ACCOUNT_EMAIL_VERIFICATION = None
 # ACCOUNT_PASSWORD_INPUT_RENDER_VALUE =False
 # ACCOUNT_PASSWORD_MIN_LENGTH = 6
 # ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
+
+SOCIALACCOUNT_PROVIDERS = {
+    'facebook': {
+        'METHOD': 'oauth2',
+        'SCOPE': ['email', 'public_profile', 'user_friends'],
+        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        'INIT_PARAMS': {'cookie': True},
+        'FIELDS': [
+            'id',
+            'email',
+            'name',
+            'first_name',
+            'last_name',
+            'verified',
+            'locale',
+            'timezone',
+            'link',
+            'gender',
+            'updated_time',
+        ],
+        'EXCHANGE_TOKEN': True,
+        'LOCALE_FUNC': 'path.to.callable',
+        'VERIFIED_EMAIL': False,
+        'VERSION': 'v2.12',
+    }
+}
